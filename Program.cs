7d362418b23a47926145afc8ad15a1f0e4fa30d9
tuan <tuan.nguyen@datics-consulting.com>
2025-03-09 17:56:53 +0000

    Initial commit - SimpleAksApi

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Force Kestrel to listen on port 8080
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080);
+});
+
+builder.Services.AddControllers();
+var app = builder.Build();
+
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
